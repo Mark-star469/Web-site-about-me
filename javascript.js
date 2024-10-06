112349bf6cf935812a59b51703d6a043ad5fd356
Mark-star469 <bayjanowmejnun0@gmail.com>
2024-10-06 14:43:50 +0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,8 @@
+window.onresize = meniň_funksiýam
+
+window.onload = meniň_funksiýam
+
+function meniň_funksiýam() {
+    a = window.innerWidth
+    document.getElementById("a").innerHTML = "Ekran Uzynlygy: " + a + "px"
+}
